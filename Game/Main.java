88f22d676ae7aff2d5297ddbb84277ec34b3089b
xianyao009 <yao009@hotmail.com>
2021-08-28 23:07:06 -0700

    Added some testing case
    Testing case added, will remove in the future

--- Game/Main.java
+++ Game/Main.java
@@ -17,32 +17,43 @@ public class Main{
         System.out.print("Enter 1 or 2:\n[1] Sword\n[2] Axe\n");
         weapon.name = scan.nextLine();
         if(weapon.name.equals("1")){
-            weapon.name = "Sword";
+            weapon.name = "Plastic Sword";
             weapon.type = "One handed sword";
             weapon.rarity = "Common";
-            weapon.details = "A sword made of plastic.";
+            weapon.description = "A sword made of plastic.";
             weapon.attack = 2;
             weapon.attackSpeed = 2;
+            System.out.println("You obtained Plastic Sword!");
         }else if(weapon.name.equals("2")){
-            weapon.name = "Axe";
+            weapon.name = "Plastic Axe";
             weapon.type = "One handed axe";
             weapon.rarity = "Common";
-            weapon.details = "An axe made of plastic.";
+            weapon.description = "An axe made of plastic.";
             weapon.attack = 3;
             weapon.attackSpeed = 1;
+            System.out.println("You obtained Plastic Axe!");
         }
+        
         character.equipWeapon(weapon);
-        //weapon.weaponInfo();
+        
     }
 
     public static void main(String[]args){
         introduction();
         Weapon weapon = new Weapon("Unknown", "Unknown", "Unknown", "Unknown", 0, 0);
         Armor armor = new Armor("Unknown", "Unknown", "Unknown", "Unknown", 0);
-        Character char1 = new Character(" ", "Human", 1, 10, weapon, armor);
+		Item item = new Item(" ", " "," "," ");
+        Character char1 = new Character(" ", "Human", 1, 10, 0, 0, 0, weapon, armor);
         createCharacter(char1, weapon, armor);
         Scanner scan = new Scanner(System.in);
-        char1.characterInfo();
+        char1.printInfo();
+        weapon.printInfo();
+		item.printInfo();
+        armor.printInfo();
+        Monster mon1 = new Monster(" ", " ", 0, 0, 0, 0, 0);
+        mon1.name = "Snail";
+        mon1.race = "aaa";
+        mon1.printInfo();
 
 
     }
