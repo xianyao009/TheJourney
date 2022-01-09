eb61a0960e0fb8bc705661a5a76d880c9fbb51ca
xianyao009 <yao009@hotmail.com>
2022-01-09 01:40:40 -0800

    Redo Monster.java: - Refactor from class to enum

--- Game/Monster.java
+++ Game/Monster.java
@@ -1,20 +1,55 @@
-class Monster extends LivingThing{
+public enum Monster {
+    GREENGOBLIN("Green goblin", "Goblin", 1, 8, 2, 2, 2),
+    REDGOBLIN("Red goblin", "Goblin", 2, 12, 3, 2, 2),
+    BLACKGOBLIN("Black goblin", "Goblin", 3, 15, 4, 3, 2),
+    GOLDGOBLIN("Gold goblin", "Goblin", 5, 20, 7, 5, 4),
+    GOBLINBOSS("Goblin boss", "Goblin", 100, 1000, 1000, 1000, 1000);
 
-    public Monster(String name, String race, int level, int health, int attack, int attackSpeed, int defense, boolean combatStatus){
-        super(name, race, level, health, attack, attackSpeed, defense, combatStatus);
+    private final String name;
+    private final String species;
+    private final int level;
+    private final int health;
+    private final int attack;
+    private final int defense;
+    private final int attackSpeed;
+
+    Monster(final String name, final String species, final int level, final int health, final int attack, final int defense, final int attackSpeed) {
+        this.name = name;
+        this.species = species;
+        this.level = level;
+        this.health = health;
+        this.attack = attack;
+        this.defense = defense;
+        this.attackSpeed = attackSpeed;
     }
 
-    @Override
-    public void printInfo(){
-        System.out.println("Monster Info");
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        System.out.println("Name: " + name);
-        System.out.println("Race: " + race);
-        System.out.println("Level: " + level);
-        System.out.println("Health: " + health);
-        System.out.println("Attack: " + attack);
-        System.out.println("Attack speed: " + attackSpeed);
-        System.out.println("Defense: " + defense);
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    public String getName() {
+        return name;
     }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public int getAttackSpeed() {
+        return attackSpeed;
+    }
+
+
 }
