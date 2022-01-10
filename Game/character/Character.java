f174fd03859f1ced3d8ff234957dcd64a4b17897
Xian Yao Ng <59724259+xianyao009@users.noreply.github.com>
2022-01-10 13:28:32 -0800

    Update Character.java

--- Game/character/Character.java
+++ Game/character/Character.java
@@ -142,7 +142,7 @@ public class Character {
      * Print out the info of character
      */
     public void printInfo() {
-        System.out.println("character.Character Info");
+        System.out.println("Character Info");
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         System.out.println("Name: " + name);
         System.out.println("Race: " + race);
