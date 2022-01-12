24866ac9640215b3a97181255a974c68060541cd
xianyao009 <yao009@hotmail.com>
2022-01-12 00:00:04 -0800

    Added @BeforeEach for redundant Character object creation

--- Game/test/CharacterTest.java
+++ Game/test/CharacterTest.java
@@ -1,5 +1,7 @@
 package test;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import character.Character;
 import equipment.Weapon;
@@ -7,38 +9,39 @@ import equipment.Armor;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CharacterTest {
+    Character character;
+
+    @BeforeEach
+    void Create_Character_Object() {
+        character = new Character();
+    }
 
     @Test
     void Check_If_Weapon_Is_Equipped() {
-        Character character = new Character();
         character.equipWeapon(Weapon.WOODENSWORD);
         assertEquals(Weapon.WOODENSWORD, character.getEquipmentTab().getWeapon());
     }
 
     @Test
     void Check_If_Armor_Is_Equipped() {
-        Character character = new Character();
         character.equipArmor(Armor.WHITETSHIRT);
         assertEquals(Armor.WHITETSHIRT, character.getEquipmentTab().getArmor());
     }
 
     @Test
     void Check_If_Attack_Is_Updated_Correctly() {
-        Character character = new Character();
         character.equipWeapon(Weapon.WOODENSWORD);
         assertEquals(Weapon.WOODENSWORD.getAttack(), character.getAttack());
     }
 
     @Test
-    void Check_If_Attackspeed_Is_Updated_Correctly() {
-        Character character = new Character();
+    void Check_If_Attack_Speed_Is_Updated_Correctly() {
         character.equipWeapon(Weapon.WOODENSWORD);
         assertEquals(Weapon.WOODENSWORD.getAttackSpeed(), character.getAttackSpeed());
     }
 
     @Test
     void Check_If_Defense_Is_Updated_Correctly() {
-        Character character = new Character();
         character.equipArmor(Armor.WHITETSHIRT);
         assertEquals(Armor.WHITETSHIRT.getDefense(), character.getDefense());
     }
