b646e5d68e21b84b5d5fef6ee030cf2a7ba0cd61
xianyao009 <yao009@hotmail.com>
2022-01-16 00:09:56 -0800

    Continue the storyline (Battle.java) and changes a few words.

--- Game/main/Story.java
+++ Game/main/Story.java
@@ -1,7 +1,9 @@
 package main;
 
 import character.Character;
+import combat.Battle;
 import equipment.Weapon;
+import monster.Monster;
 
 import java.util.*;
 
@@ -11,7 +13,7 @@ import java.util.*;
  */
 public class Story {
 	Main main;
-	String temp = "";	// a temporary variable to store the choice of player
+	String choice = "";	// a temporary variable to store the choice of player
 	String text;	// text to print
 	
 	Character character = new Character();
@@ -29,41 +31,66 @@ public class Story {
 		text = "Welcome to The Journey.\nFirst, please tell me your name.\nName: ";
 		delayPrint(50);
 		character.setName(scan.nextLine());
-		text = "\nHi " + character.getName() + ", nice name! Now pick your weapon!\n";
+		text = "\nNice to meet you, " + character.getName() + "! Now pick your weapon!\n";
 		delayPrint(50);
 
+		label:
 		do {
 			text = "Do you want a sword or an axe?\nEnter '1' to pick \"Wooden sword\", '2' to pick \"Wooden axe\" or '3' to view the stats of the weapons: ";
 			delayPrint(50);
-			temp = scan.nextLine();
-			if (temp.equals("1")) {
-				character.getInventory().addWeapon(Weapon.WOODENSWORD);
-				character.equipWeapon(Weapon.WOODENSWORD);
-				break;
-			} else if (temp.equals("2")) {
-				character.getInventory().addWeapon(Weapon.WOODENAXE);
-				character.equipWeapon(Weapon.WOODENAXE);
-				break;
-			} else if (temp.equals("3")) {
-				Weapon.WOODENSWORD.printInfo();
-				Weapon.WOODENAXE.printInfo();
-			} else {
-				System.out.println("\nPlease enter '1', '2' or '3' only!");
+			choice = scan.nextLine();
+			switch (choice) {
+				case "1":
+					character.getInventory().addWeapon(Weapon.WOODENSWORD);
+					character.equipWeapon(Weapon.WOODENSWORD);
+					break label;
+				case "2":
+					character.getInventory().addWeapon(Weapon.WOODENAXE);
+					character.equipWeapon(Weapon.WOODENAXE);
+					break label;
+				case "3":
+					Weapon.WOODENSWORD.printInfo();
+					Weapon.WOODENAXE.printInfo();
+					break;
+				default:
+					System.out.println("\nPlease enter '1', '2' or '3' only!");
+					break;
 			}
 		} while (true);
-		
+
 		text = "\nNice choice! Now, go take some rest. You'll start your journey the next day.";
 		delayPrint(50);
     }
 	
-	// Main.Main body of the game
+	// Main body of the game
 	public void body() {
-		text = ".\n.\n.\n.\n.\n.\n.\n";
-		delayPrint(250);
-		text = "You encountered a Goblin!";
+		Scanner scan = new Scanner(System.in);
+		text = ".\n.\n.\n.\n.\n.\n";
+		delayPrint(200);
+		text = "You encountered a Green goblin!";
 		delayPrint(50);
-		System.out.println("\nWill implement the Combat.Battle class soon....");
-		
+		Battle battle = new Battle(character, Monster.GREENGOBLIN);
+		while(battle.battleStatus()) {
+			battle.printBattleDetails();
+			text = "What will you do?\n1. Attack | 2. Inventory\nEnter '1' to attack or '2' to open your inventory: ";
+			delayPrint(50);
+			choice = scan.nextLine();
+			if (choice.equals("1")) {
+				text = battle.attack();
+				delayPrint(50);
+			} else if (choice.equals("2")) {
+				//battle.openInventory();
+				text = "Will implement the open inventory feature in the future...\n";
+				delayPrint(50);
+				break;
+			}
+		}
+		if (!battle.battleStatus()) {
+			text = battle.died() + " died!\n";
+			delayPrint(50);
+		}
+		text = "It's 11:52pm.... I need to go to bed... By Xian Yao Ng";
+		delayPrint(50);
 	}
 
 	/**
