98d3541756ffd86c9c539e567cd95612f1ea1a11
xianyao009 <yao009@hotmail.com>
2022-01-17 00:08:09 -0800

    Implemented Potion.class

--- Game/item/Potion.java
+++ Game/item/Potion.java
@@ -1,4 +1,30 @@
 package item;
 
 public enum Potion {
+    HEALTHPOTION("Health potion", "Recovers 10 health", 5),
+    SUPERPOTION("Super potion", "Recovers 20 health", 10),
+    ULTRAPOTION("Ultra potion", "Fully recovers your health", 9999999);
+
+    private final String name;
+    private final String effect;
+    private final int amount;
+
+
+    Potion(final String name, final String effect, final int amount) {
+        this.name = name;
+        this.effect = effect;
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEffect() {
+        return effect;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
 }
